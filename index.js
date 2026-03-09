cb996940d3eeaf96df81b6cbdf743ac8b4f8d349
Aki Peltokangas <v0peak00@students.oamk.fi>
2026-03-09 09:20:57 +0200

    remove cors

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
 
 let notes = [
   {
