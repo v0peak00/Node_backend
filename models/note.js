61b265d9d9320c9c0c6a5bc93d52272c118c3175
Aki Peltokangas <v0peak00@students.oamk.fi>
2026-03-10 15:10:15 +0200

    change URI to NoteApp URI

--- models/note.js
+++ models/note.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI_NOTEAPP
 console.log('connecting to', url)
 mongoose.connect(url, { family: 4 })
   .then(result => {
