e4d5abd08fc6cfd8bd1966b0b98da63884d4aa3f
Aki Peltokangas <v0peak00@students.oamk.fi>
2026-03-10 11:56:45 +0200

    Person model otettu käyttöön ja polut muokattu sen mukaisesti

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -13,40 +15,12 @@ app.use(morgan('tiny', {skip: function (req, res) { return req.method === 'POST'
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :personObject'))
 
-let persons = [
-  {
-    id: "1",
-    name: "Matti Meikäläinen",
-    number: "0401234567"
-  },
-  {
-    id: "2",
-    name: "Liisa Virtanen",
-    number: "0509876543"
-  },
-  {
-    id: "3",
-    name: "Pekka Korhonen",
-    number: "0451122334"
-  },
-  {
-    id: "4",
-    name: "Anna Laine",
-    number: "0415566778"
-  },
-  {
-    id: "5",
-    name: "Jussi Nieminen",
-    number: "0449988776"
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-  const count = persons.length
+  const count = 5
   const date = new Date(Date.now()).toString()
 
   response.send(`
@@ -57,17 +31,15 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((people) => {
+    response.json(people)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find((person) => person.id === id)
-  if(person) {
-      response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person.findById(request.params.id).then((person) => {
+    response.json(person)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -82,33 +54,30 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-  const randomNumber = Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))
-  const id = randomNumber.toString()
   const person = request.body
   
   if(!person.name || !person.number) {
     return response.status(400).json({error: "Missing name or number!"})
   } 
-  const nameExists = persons.some(p =>  p.name === person.name)
-  if(nameExists){
-    return response.status(400).json({error: "Name already exists!"})
-  }
-  const personWithId = {id: id, ...person}
-  persons = [...persons, personWithId]
-  response.status(201).json(personWithId)
+  const dbPerson = new Person({
+    name: person.name,
+    number: person.number
+  })
+
+  dbPerson.save().then((result) => {
+    console.log("New Person entry created!")
+    response.status(201).json(result)
+  })
+  
 })
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-const personCountandIds = () => {
-  let ids = []
-  ids = persons.map(person => ids.concat(person.id))
-  console.log("persons arr length: " + persons.length + " and id's: " + ids)
-}
-
 app.use(unknownEndpoint)
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
