525ccf0eba1a49660f7ef78db4c11be1d2c3a58e
Aki Peltokangas <v0peak00@students.oamk.fi>
2026-03-10 08:14:25 +0200

    Add mongoDB

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://akipelto0_db_user:${password}
+@cluster0.lzuenmy.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'JS is both easy and hard',
+  important: true,
+})
+
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
