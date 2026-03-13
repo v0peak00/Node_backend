4e089e00e98c2a55b1028705e53724e1d54a295f
Aki Peltokangas <v0peak00@students.oamk.fi>
2026-03-13 11:32:49 +0200

    ehdollinen polku otettu käyttöön

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URI, { family: 4 })
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
